53310e950d0c29e27cc5a535a02a6e32f8fa073f
이의태 <lutae2000@gmail.com>
2026-09-21 17:29:59 +0900

    git 상태 수정

--- pdfToExcel/build_exe.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0build_exe.ps1"
-pause
--- pdfToExcel/run_pdf_to_excel.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-if exist "dist\PdfToExcel.exe" (
-    start "" "%~dp0dist\PdfToExcel.exe"
-) else (
-    echo EXE file not found. Run build_exe.ps1 first.
-    pause
-)
